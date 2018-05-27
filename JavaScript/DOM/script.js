184db1ad2c2bc9a2b73658be70a0047e14a4a496
David Salazar <david.asal@hotmail.com>
2018-05-27 13:33:31 -0500

    added more dom stuff

--- JavaScript/DOM/script.js
+++ JavaScript/DOM/script.js
@@ -1,4 +1,37 @@
-var tag = document.getElementById("highlight");
-var tags = document.getElementsByClassName("bolded");
+// var tag = document.getElementById("highlight");
+// var tags = document.getElementsByClassName("bolded");
+//
+//
+//
+// var par = document.querySelector("p");
+//
+// par.textContent = "blah blah blah";
+//
+//
+// // time to manipulate attributes
+//
+// var link = document.querySelector("a");
+// link.getAttribute("href");
+// link.setAttribute("href", "https://www.facebook.com");
+//
+// // =========time to learn events=========
+// var button = document.querySelector("button");
+// var p = document.querySelector("#content");
+//
+// button.addEventListener("click", function () {
+//   p.textContent = "What";
+//
+// });
 
-tag.style.color = "blue";
+
+
+var blue = document.querySelector(".blue");
+var red = document.querySelector(".red");
+
+red.addEventListener("click", function () {
+  red.style.background = "purple";
+});
+
+blue.addEventListener("click", function () {
+  blue.style.background = "gold";
+});
